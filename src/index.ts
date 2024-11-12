3fe52d9974c2d84963e05d8419a43463551522de
vivek <chimnanivivek14@gmail.com>
2024-11-12 09:57:39 +0530

    added routes and schemas

---
+++ src/index.ts
@@ -0,0 +1,69 @@
+import { createRoute, OpenAPIHono } from "@hono/zod-openapi";
+import { ParamSchema } from "./inputs";
+import { UserSchema } from "./outputs";
+
+const app = new OpenAPIHono();
+
+const getUserRoute = createRoute({
+  method: "get",
+  path: "/user/{id}",
+  request: {
+    params: ParamSchema,
+  },
+  responses: {
+    200: {
+      content: {
+        "application/json": {
+          schema: UserSchema,
+        },
+      },
+      description: "Get the user details",
+    },
+  },
+});
+
+const postUserRoute = createRoute({
+  method: "post",
+  path: "/user/{id}",
+  request: {
+    params: ParamSchema,
+  },
+  responses: {
+    200: {
+      content: {
+        "application/json": {
+          schema: UserSchema,
+        },
+      },
+      description: "Get the user details",
+    },
+  },
+});
+
+app.openapi(getUserRoute, (c) => {
+  const { id } = c.req.valid("param");
+  return c.json({
+    id,
+    age: 12,
+    name: "something something",
+  });
+});
+
+app.openapi(postUserRoute, (c) => {
+  const { id } = c.req.valid("param");
+  return c.json({
+    id,
+    age: 12,
+    name: "something something",
+  });
+});
+
+app.doc("/doc", {
+  openapi: "3.0.0",
+  info: {
+    version: "1.0.0",
+    title: "My HONO API",
+  },
+});
+
+export default app;
